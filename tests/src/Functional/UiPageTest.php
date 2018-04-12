accf4d46f6f50d38cebf2bc632713887852ea429
Eli Stone <eli.stone@iedigital.co.uk>
2018-04-12 22:15:04 +0100

    Added 4 tests for permissions & config

--- tests/src/Functional/UiPageTest.php
+++ tests/src/Functional/UiPageTest.php
@@ -17,7 +17,7 @@ class UiPageTest extends BrowserTestBase {
    *
    * @var array
    */
-  protected static $modules = ['simple_a_b'];
+  protected static $modules = ['views','simple_a_b'];
 
 
   /**
@@ -31,13 +31,74 @@ class UiPageTest extends BrowserTestBase {
    * Tests that a/b listing page works.
    */
   public function testSimpleABListingPage() {
+    // create a user with permission to create ab tests
     $account = $this->drupalCreateUser(['create ab tests']);
+    // login the drupal user
     $this->drupalLogin($account);
 
+    // navigate to simple-a-b listing page
     $this->drupalGet('/admin/config/user-interface/simple-a-b');
+    // check we have permission to view the page
     $this->assertSession()->statusCodeEquals(200);
 
-//    $this->assertSession()->pageTextContains('This is a list of available A/B tests.');
+    // check that we can find the view the output on A/B listing page
+    $this->assertSession()->pageTextContains('No A/B tests have been found.');
+  }
+
+
+  /**
+   * Tests a/b listing without permission.
+   */
+  public function testSimpleABListingPageWithoutPermission(){
+    // create a user with the wrong permissions to edit settings
+    $account = $this->drupalCreateUser(['configure ab tests']);
+    // login the drupal user
+    $this->drupalLogin($account);
+
+    // navigate to simple-a-b settings page
+    $this->drupalGet('/admin/config/user-interface/simple-a-b');
+    // check we don't have permission to view the page
+    $this->assertSession()->statusCodeEquals(403);
+  }
+
+
+  /**
+   * Tests that a/b settings page works.
+   */
+  public function testSimpleABSettingsPage() {
+    // create a user with permission to configure ab tests
+    $account = $this->drupalCreateUser(['configure ab tests']);
+    // login the drupal user
+    $this->drupalLogin($account);
+
+    // navigate to simple-a-b settings page
+    $this->drupalGet('/admin/config/user-interface/simple-a-b/settings');
+    // check we have permission to view the page
+    $this->assertSession()->statusCodeEquals(200);
+
+    // check that we can find the view the output on A/B settings
+    $this->assertSession()->pageTextContains('Settings to configure how simple a/b handles reporting.');
+
+    // try to save the settings config
+    $this->getSession()->getPage()->pressButton('Save configuration');
+
+    // check if we get a response from pressing the save button
+    $this->assertSession()->pageTextContains('The configuration options have been saved.');
+  }
+
+  /**
+   * Tests a/b settings without permission.
+   */
+  public function testSimpleABSettingsPageWithoutPermission(){
+    // create a user with the wrong permissions to edit settings
+    $account = $this->drupalCreateUser(['create ab tests']);
+    // login the drupal user
+    $this->drupalLogin($account);
+
+    // navigate to simple-a-b settings page
+    $this->drupalGet('/admin/config/user-interface/simple-a-b/settings');
+    // check we don't have permission to view the page
+    $this->assertSession()->statusCodeEquals(403);
   }
 
 }
